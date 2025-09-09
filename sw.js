1c98e06ea5fa59d6719b6c808850f79a78c1da9f
vertomaker <eric_jpn@live.com>
2025-09-09 15:01:30 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,22 @@
+// Versão 1.1
 self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open("calc3d-store").then((cache) => {
-      return cache.addAll([
-        "index.html",
-        "manifest.json",
-        "icons/icon-192.png",
-        "icons/icon-512.png"
-      ]);
-    })
-  );
+    e.waitUntil(
+        caches.open("calc3d-store").then((cache) => {
+            return cache.addAll([
+                "index.html",
+                "manifest.json",
+                "icons/icon-192.png",
+                "icons/icon-512.png"
+            ]);
+        })
+    );
 });
 
 self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
-  );
+    e.respondWith(
+        caches.match(e.request).then((response) => {
+            return response || fetch(e.request);
+        })
+    );
 });
+
