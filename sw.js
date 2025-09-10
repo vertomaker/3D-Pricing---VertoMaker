a7cbcadbfae701f16d69e0daae46fc2d99c67098
vertomaker <eric_jpn@live.com>
2025-09-09 23:50:55 -0300

    Atualizar o sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,10 @@
-// Versão 1.1
+// Versão 1.6
 self.addEventListener("install", (e) => {
     e.waitUntil(
         caches.open("calc3d-store").then((cache) => {
             return cache.addAll([
                 "index.html",
+                "script.js",
                 "manifest.json",
                 "icons/icon-192.png",
                 "icons/icon-512.png"
