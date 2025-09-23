a440deedbfa2b0153a9f8c24dc91202dfadd7f44
vertomaker <eric_jpn@live.com>
2025-09-23 17:54:31 -0300

    Update sw.js ver.3.2.1

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Nome do cache principal, atualize para forçar a nova versão
-const CACHE_NAME = 'calc3d-store-v3.2';
+const CACHE_NAME = 'calc3d-store-v3.2.1';
 
 // Lista de arquivos essenciais que serão pré-armazenados em cache
 const urlsToCache = [
